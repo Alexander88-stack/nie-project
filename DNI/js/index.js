19fd0228e781c4126b773c3f209926d02e113a22
Commit <>
2020-10-08 12:35:13 +0200

    empezando a coger hilo de java script

--- DNI/js/index.js
+++ DNI/js/index.js
@@ -1,3 +1,53 @@
 console.log("Hola mundo!")
 
-const casse = getElementById(nif)
+function comprobacion() {
+    const letras = ["T","R","W","A","G","M","Y","F","P","D","B","N","J","Z","S","Q","V","H","L","C","K","E"];
+    
+    let juntarDni = document.getElementById(nif).value;
+    const i = 0;
+    let identificador= 0;
+    let separarDni = ' ';
+    let resto;
+    
+    let dnimayus = '';
+}
+    for (i = 0 ; i < separarDni().length ; i ++) {
+
+        if (i <=7){
+            if (isNaN(separarDni(i))) {
+                identificador = 1;
+            }
+        }
+        else{
+            if (isNaN(separarDni[8])){
+                separarDni[8] = separarDni[8].toUpperCase();
+            }
+            else{
+                identificador = 1;
+            }
+
+
+        }
+    }
+    if (i !=9){
+        identificador = 1;
+    }
+    else{
+        dnimayus = separarDni.join("");
+        numerosdeldni = dnimayus.substring[0,8];
+        letrafinal= dnimayus.substring[9];
+        resto = numerosdeldni / 23;
+        if(vector[resto] != letrafinal) {
+            identificador = 2;
+        }
+        }
+
+    if (identificador == 0){
+        alert ("DNI correcto");
+    }
+    else if (identificador == 1){
+        alert("DNI incorrecto");
+    }
+    else if (identificador == 2){
+        alert("Letra erronea");
+    }
